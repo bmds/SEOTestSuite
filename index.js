d74c1ea06b22e3266c9515fdcabf875ef8d88e38
tawashley <trisashley@gmail.com>
2017-07-18 12:40:10 +0100

    feat: Add non-social e.g meta to seo object, refactor name of social meta object to help avoud confusion

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ const seo = {
 		return images.every(theImage => theImage.hasAttribute('alt'));
 	},
 	socialMeta: () => {
-		let meta = {};
+		let socialMeta = {};
 		Array.from(document.getElementsByTagName('meta')).forEach(tag => {
 			if(tag.name && tag.name.startsWith('twitter')) {
-				meta[tag.name] = tag.content;
+				socialMeta[tag.name] = tag.content;
 			} else if(tag.attributes.property && tag.attributes.property.value.startsWith('og')) {
-				meta[tag.attributes.property.value] = tag.content;
+				socialMeta[tag.attributes.property.value] = tag.content;
 			}
 		});
+		return socialMeta;
+	},
+	meta: () => {
+		let meta = {};
+
+		Array.from(document.getElementsByTagName('meta')).forEach(tag => {
+			if(tag.name && (tag.name.startsWith('twitter') === false) && (tag.attributes.property.value.startsWith('og') === false)) {
+				meta[tag.name] = tag.content;
+			}
+		});
+
 		return meta;
 	}
 };
